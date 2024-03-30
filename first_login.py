c2dd35ad066ffd75fde085edaa70041a1a18e750
Jhairzp_27 <jhairzp27@hotmail.com>
2024-03-29 22:19:21 -0500

    Add first login

---
+++ first_login.py
@@ -0,0 +1,8 @@
+import flet as ft
+
+def main():
+    print("Hello world")
+    
+
+if __name__ == "__name__":
+    main()
--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,14 @@
 def main():
     print("Hello, world!")
     
+    numero = 5
+    hola = "HOla"
+    hola = 'HOla'
+    if numero==5:
+        print(hola)
+    else:
+        print(numero)
+    
+
 if __name__ == "__main__":
     main()
